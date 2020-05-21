f38419fee3421d06a56def12a367a0d7f411d95c
kavya654 <kavya.narayan654@gmail.com>
2020-05-21 10:19:07 +0530

    Revert "Twitter"
    This reverts commit e60452bf

--- src/main/java/com/spring/mvc/practice/twitter/WebSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.spring.mvc.practice.twitter;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- src/main/java/com/spring/mvc/practice/twitter/controller/exception/UserObjectIsNullException.java
+++
@@ -1,5 +0,0 @@
-package com.spring.mvc.practice.twitter.controller.exception;
-
-public class UserObjectIsNullException extends RuntimeException {
-
-}
